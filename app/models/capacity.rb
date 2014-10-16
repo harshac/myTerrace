4827a086286421484451ee39ae0cbf294dfcd377
Harshac <harshac@thoughtworks.com>
2014-10-17 01:29:27 +0530

    Getting location and capacities from model

---
+++ app/models/capacity.rb
@@ -0,0 +1,5 @@
+class Capacity
+  def self.all
+    [['0 to 10 persons', 10], ['11 to 20 persons', 20], ['21 to 30 persons', 30]]
+  end
+end
---
+++ app/models/location.rb
@@ -0,0 +1,5 @@
+class Location
+  def self.all
+    %w(location1 location2 location3)
+  end
+end
